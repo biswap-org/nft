bb56d1144623de52eb9b781661a188d9d03c2330
ivicmad <victor.l@lineir.me>
2021-11-08 09:34:44 +0200

    Add bsw mining contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -61,6 +61,15 @@ module.exports = {
         }
       },
       {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        }
+      },
+      {
         version: "0.8.4",
         settings: {
           optimizer: {
--- scripts/deployLaunchpadForBSW.js
+++ scripts/deployLaunchpadForBSW.js
@@ -7,37 +7,38 @@ const treasuryAddress = `0x6332Da1565F0135E7b7Daa41C419106Af93274BA`;
 const dealTokenAddress = `0x965f527d9159dce6288a2219db51fc6eef120dd1`;
 const biswapNFTAddress = `0xD4220B0B196824C2F548a34C47D81737b0F6B5D6`;
 
+const launchpadAddress = `0x91C3Ec814597D41BB974e60672ec2a2cd4b14da9`;
 let launchpad;
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log(`Deployer address: ${ deployer.address}`);
-  let nonce = await network.provider.send(`eth_getTransactionCount`, [deployer.address, "latest"]);
-
-  console.log(`Start deploy launchpad NFT`);
-  const Launchpad = await ethers.getContractFactory('LaunchpadNftForBSW');
-  launchpad = await Launchpad.deploy(biswapNFTAddress, dealTokenAddress, treasuryAddress, {nonce: nonce});
-  await launchpad.deployTransaction.wait();
-  console.log(`Launchpad NFT deployed to ${launchpad.address}`);
-
-
-
-  console.log(`Setup roles`);
-  let BiswapNFT = await ethers.getContractFactory(`BiswapNFT`);
-  let biswapNft = await BiswapNFT.attach(biswapNFTAddress);
-
-  const LAUNCHPAD_TOKEN_MINTER = await biswapNft.LAUNCHPAD_TOKEN_MINTER();
-  let tx = await biswapNft.grantRole(LAUNCHPAD_TOKEN_MINTER, launchpad.address, {nonce: ++nonce, gasLimit: 3000000});
-  await tx.wait();
-  if(await biswapNft.hasRole(LAUNCHPAD_TOKEN_MINTER, launchpad.address)){
-    console.log(`Role LAUNCHPAD_TOKEN_MINTER successfully added to address ${launchpad.address}`);
-  } else{
-    console.log(`WARNING!!! Role not added!!!`);
-  }
-
+  // const [deployer] = await ethers.getSigners();
+  // console.log(`Deployer address: ${ deployer.address}`);
+  // let nonce = await network.provider.send(`eth_getTransactionCount`, [deployer.address, "latest"]);
+  //
+  // console.log(`Start deploy launchpad NFT`);
+  // const Launchpad = await ethers.getContractFactory('LaunchpadNftForBSW');
+  // launchpad = await Launchpad.deploy(biswapNFTAddress, dealTokenAddress, treasuryAddress, {nonce: nonce});
+  // await launchpad.deployTransaction.wait();
+  // console.log(`Launchpad NFT deployed to ${launchpad.address}`);
+  //
+  //
+  //
+  // console.log(`Setup roles`);
+  // let BiswapNFT = await ethers.getContractFactory(`BiswapNFT`);
+  // let biswapNft = await BiswapNFT.attach(biswapNFTAddress);
+  //
+  // const LAUNCHPAD_TOKEN_MINTER = await biswapNft.LAUNCHPAD_TOKEN_MINTER();
+  // let tx = await biswapNft.grantRole(LAUNCHPAD_TOKEN_MINTER, launchpad.address, {nonce: ++nonce, gasLimit: 3000000});
+  // await tx.wait();
+  // if(await biswapNft.hasRole(LAUNCHPAD_TOKEN_MINTER, launchpad.address)){
+  //   console.log(`Role LAUNCHPAD_TOKEN_MINTER successfully added to address ${launchpad.address}`);
+  // } else{
+  //   console.log(`WARNING!!! Role not added!!!`);
+  // }
+  //
   console.log(`Verify contract`);
   let res = await hre.run("verify:verify", {
-    address: launchpad.address,
+    address: launchpadAddress,
     constructorArguments: [biswapNFTAddress, dealTokenAddress, treasuryAddress]
   })
   console.log(res);
