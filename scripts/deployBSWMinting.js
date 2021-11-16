07d66c28540969c9061fbec3352c2a334dd5bb2e
ivicmad <victor.l@lineir.me>
2021-11-16 19:14:24 +0200

    Add bswMinting deploy script

--- scripts/deployBSWMinting.js
+++ scripts/deployBSWMinting.js
@@ -3,11 +3,11 @@ const { ethers, network } = require(`hardhat`);
 const {BigNumber} = require("ethers");
 
 
-const swapFeeRewardAddress = ``;
-const nftStakingAddress = ``;
-const swapFeeRewardMintingPerBlock = expandTo18Decimals(0);
-const nftStakingMintingPerBlock = expandTo18Decimals(0);
-const maxMint = expandTo18Decimals(0);
+const swapFeeRewardAddress = `0xFaA9d8BB498ce62c1568170bA706cFeBB9B9ec19`;
+const nftStakingAddress = `0x586f7Aac5A3B111d6Acf59f1B0aCa2d72C91223d`;
+const swapFeeRewardMintingPerBlock = '1041666666666666666';
+const nftStakingMintingPerBlock = '198412698400000000';
+const maxMint = expandTo18Decimals(60000000);
 const bswToken = `0x965f527d9159dce6288a2219db51fc6eef120dd1`;
 
 function expandTo18Decimals(n) {
@@ -16,7 +16,7 @@ function expandTo18Decimals(n) {
 
 async function main() {
     let accounts = await ethers.getSigners();
-    console.log(`Deployer address: ${ accounts[0].address}`);
+    console.log(`Deployer address: ${ accounts[0].address }`);
     let nonce = await network.provider.send(`eth_getTransactionCount`, [accounts[0].address, "latest"]);
 
     const BswMint = await ethers.getContractFactory(`BSWMinting`);
