bb146420d8d893ed94e0ea626a55f12d4b299406
ivicmad <victor.l@lineir.me>
2022-03-30 11:32:09 +0300

    NFT mint script

--- scripts/mintNFTTokens.js
+++ scripts/mintNFTTokens.js
@@ -1,16 +1,14 @@
 //npx hardhat run scripts/mintNFTTokens.js --network mainnetBSC
 const { ethers, network } = require(`hardhat`);
-const {BigNumber} = require("ethers");
 
 const toBN = (numb, power) =>  ethers.BigNumber.from(10).pow(power).mul(numb);
 
 const biswapNFTAddress = `0xD4220B0B196824C2F548a34C47D81737b0F6B5D6`
-const to = `0xe843116517809C0d59c8a19dA4f7684f1d34433B`
-const level = 1
-const rb = toBN(1,18);
+const to = `0xe4a922f8fDAb0Fba636766c0C8C3fD864845163f`
+
 
 let biswapNft
-
+//Task: BSW-1731
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log(`Deployer address: ${ deployer.address}`);
@@ -19,11 +17,16 @@ async function main() {
     console.log(`mint NFT Tokens`);
     const BiswapNFT = await ethers.getContractFactory(`BiswapNFT`);
     biswapNft = await BiswapNFT.attach(biswapNFTAddress);
-    for(let i = 0; i < 20; i++){
-        await biswapNft.launchpadMint(to, level, rb, {nonce: ++nonce, gasLimit: 1e6});
-        console.log(`token ${+i+1} minted`);
-    }
-
+    await biswapNft.launchpadMint(to, 2, toBN(66,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(8,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(7,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(6,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(5,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
+    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
 }
 
 main()
