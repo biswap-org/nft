5d4fcd345dc26781e49a31c69212e4b24e9283c4
ivicmad <victor.l@lineir.me>
2022-04-04 15:22:57 +0300

    NFT mint script

--- scripts/mintNFTTokens.js
+++ scripts/mintNFTTokens.js
@@ -4,29 +4,39 @@ const { ethers, network } = require(`hardhat`);
 const toBN = (numb, power) =>  ethers.BigNumber.from(10).pow(power).mul(numb);
 
 const biswapNFTAddress = `0xD4220B0B196824C2F548a34C47D81737b0F6B5D6`
-const to = `0xe4a922f8fDAb0Fba636766c0C8C3fD864845163f`
+const to = `0xe843116517809C0d59c8a19dA4f7684f1d34433B`
+const bsPlayersAddress = `0xb00ED7E3671Af2675c551a1C26Ffdcc5b425359b`
+const bsBusesAddress = '0x6d57712416eD4890e114A37E2D84AB8f9CEe4752'
 
+const TOKEN_MINTER_ROLE = '0x262c70cb68844873654dc54487b634cb00850c1e13c785cd0d96a2b89b829472'
 
-let biswapNft
-//Task: BSW-1731
+let biswapNft, bsplayers, bsbuses
+
+//Task: BSW-1779
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log(`Deployer address: ${ deployer.address}`);
     let nonce = await network.provider.send(`eth_getTransactionCount`, [deployer.address, "latest"]) - 1;
 
-    console.log(`mint NFT Tokens`);
     const BiswapNFT = await ethers.getContractFactory(`BiswapNFT`);
     biswapNft = await BiswapNFT.attach(biswapNFTAddress);
-    await biswapNft.launchpadMint(to, 2, toBN(66,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(8,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(7,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(6,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(5,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
-    await biswapNft.launchpadMint(to, 1, toBN(4,18), {nonce: ++nonce, gasLimit: 5e6});
+    bsplayers = await ethers.getContractAt(require(`../ABIs/bspContract.json`), bsPlayersAddress);
+    bsbuses = await ethers.getContractAt(require(`../ABIs/bsbContract.json`), bsBusesAddress);
+
+    console.log(`Set minter role to buses contract`);
+    await bsbuses.grantRole(TOKEN_MINTER_ROLE, deployer.address, {nonce: ++nonce, gasLimit: 5e6});
+
+    console.log(`Mint tokens:`);
+    console.log(`   - Mint Robi token`);
+    await biswapNft.launchpadMint(to, 1, toBN(1,18), {nonce: ++nonce, gasLimit: 5e6});
+
+    console.log(`   - Mint Bus token`);
+    await bsbuses.mint(to, 2, {nonce: ++nonce, gasLimit: 5e6});
+
+    console.log(`   - Mint Player token`);
+    await bsplayers.mint(to, toBN(1000, 18), 0, 1, {nonce: ++nonce, gasLimit: 5e6});
+
+    console.log(`Done`);
 }
 
 main()
