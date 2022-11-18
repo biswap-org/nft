8b84659a3ea33d2e2cf433f88c921e9afec919b9
ivicmad <victor.l@lineir.me>
2022-11-18 15:17:18 +0200

    Mint NFT Tokens script for test

--- scripts/mintNFTTokens.js
+++ scripts/mintNFTTokens.js
@@ -1,32 +1,30 @@
 //npx hardhat run scripts/mintNFTTokens.js --network mainnetBSC
 const { ethers, network } = require(`hardhat`);
+const {BigNumber} = require("ethers");
 
 const toBN = (numb, power) =>  ethers.BigNumber.from(10).pow(power).mul(numb);
 
 const biswapNFTAddress = `0xD4220B0B196824C2F548a34C47D81737b0F6B5D6`
-const to = `0xe843116517809C0d59c8a19dA4f7684f1d34433B`
+const to = `0xF0aECBad150185Be71c6F3BEF40BB58F4CC5C8cf`
 
-//Task: BSW-1928
+
+let biswapNft
+
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log(`Deployer address: ${ deployer.address}`);
     let nonce = await network.provider.send(`eth_getTransactionCount`, [deployer.address, "latest"]) - 1;
 
+    console.log(`mint NFT Tokens`);
     const BiswapNFT = await ethers.getContractFactory(`BiswapNFT`);
-    const biswapNft = await BiswapNFT.attach(biswapNFTAddress);
-
-    console.log(`Mint tokens:`);
-    for(let i = 0; i < 10; i++){
-        await biswapNft.launchpadMint(to, 1, toBN(1,18), {nonce: ++nonce, gasLimit: 5e6});
-        console.log(`   - Mint Robi token ${i+1}`);
-    }
-
-    console.log(`Done`);
+    biswapNft = await BiswapNFT.attach(biswapNFTAddress);
+    for(let i =0; i < 10; i++ ) await biswapNft.launchpadMint(to, 1, toBN(10,18), {nonce: ++nonce, gasLimit: 5e6});
+    console.log(await biswapNft.balanceOf(to));
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+      console.error(error);
+      process.exit(1);
+  });
