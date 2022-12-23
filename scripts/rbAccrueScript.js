910ef790e15410f6bb78f75ae193ff4954c9b81c
ivicmad <victor.l@lineir.me>
2022-12-23 15:29:18 +0200

    BSW-3050:ADD:Script accrue RB

--- scripts/rbAccrueScript.js
+++ scripts/rbAccrueScript.js
@@ -4,12 +4,12 @@ const { ethers, network } = require(`hardhat`);
 const toBN = (numb, power) =>  ethers.BigNumber.from(10).pow(power).mul(numb);
 
 const biswapNFTAddress = `0xD4220B0B196824C2F548a34C47D81737b0F6B5D6`
-const to = `0x04F2DdF4FA327323202a9B8714a173D7Af0fE6a0`
+const to = `0xF0aECBad150185Be71c6F3BEF40BB58F4CC5C8cf`
 
 
 const RB_SETTER_ROLE = '0xc7c9819f33f023fb575ae9b63a0181942ca5956a309f3641e15d6dc199033e46'
 
-let biswapNft, bsplayers, bsbuses
+let biswapNft
 
 
 async function main() {
